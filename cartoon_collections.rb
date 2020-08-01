9256bd6b03d055a50d390e9a9271f29d1d042c89
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-08-01 16:00:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,9 +11,7 @@ def summon_captain_planet (array)
 end
 
 def long_planeteer_calls(array)
-  array.all? do |call|
-    call.length < 4
-  end
+  return false if array.all? {|call| call.length < 4}
 end
 
 def find_the_cheese (array)
