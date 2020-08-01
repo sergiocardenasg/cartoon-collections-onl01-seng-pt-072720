5896c0b427d1223b71a21c9f4c078a68ea66128e
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-08-01 15:55:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,6 @@ end
 
 def find_the_cheese (array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.include?(cheese_types)
-    return array.find {|i| }
+  if 
+    return array.find {|i|i.include?(cheese_types) }
 end
